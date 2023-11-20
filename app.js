66e6318a8ff899888b3ae2fba702020380cc44fa
Riadh Gharbi <riadhgharbi96@gmail.com>
2023-11-20 14:56:47 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// app.js
+ const express = require('express');
+ const app = express();
+app.get('/', (req, res) => {
+ res.send('Hello Azure!');
+ });
+const port = process.env.PORT || 3000;
+ app.listen(port, () => {
+ console.log(`Server is running on port ${port}`);
+ });
